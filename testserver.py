053d4951153b9374a323da536a300613626cec40
TamarHalali <tyh444222@gmail.com>
2025-11-19 08:00:21 -0500

    beezrat hashemmm

--- testserver.py
+++ testserver.py
@@ -1,15 +1,23 @@
-import time
+# test_flask_app.py
 import requests
+import sys
 
-url = "http://app:5000/add"
-data = {"x":1}
+def test_add_item():
+    url = "http://flask_app:5001/add"  # זה ה-service name מ-Docker Compose
+    data = {"title": "Buy groceries"}
 
-for i in range(10):
-    try:
-        response = requests.post(url, data=data)
-        print(response.text)
-        break
-    except requests.exceptions.ConnectionError:
-        print("Server not ready, waiting 2 seconds...")
-        time.sleep(2)
+    try:
+        response = requests.post(url, data=data)
+    except Exception as e:
+        print(f"Request failed: {e}")
+        sys.exit(1)
 
+    if response.status_code in [200, 201]:
+        print(f"Success! Status code: {response.status_code}")
+        sys.exit(0)
+    else:
+        print(f"Failed! Status code: {response.status_code}")
+        sys.exit(1)
+
+if _name_ == "_main_":
+    test_add_item()
